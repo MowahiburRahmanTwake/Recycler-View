342c4e05a0de4ae3bfdf2f3338b159321682e36b
Md. Mowahibur Rahman Twake <twake.edu@gmail.com>
2024-10-16 20:30:20 +0600

    Recycler View

---
+++ app/src/main/java/com/twakeapps/recyclerview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.twakeapps.recyclerview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.twakeapps.recyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+    }
+}
